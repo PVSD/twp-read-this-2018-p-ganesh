fa2cb1537d19e8feabcb2cb1b61bcb4cddf23a4b
p-ganesh <41019754+p-ganesh@users.noreply.github.com>
2018-11-05 11:17:48 -0500

    this sucks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String fileName = "Imports.txt";
+        String line = null;
+
+        File students = new File("C:\\Users\\prgan\\IdeaProjects\\twp-read-this-2018-p-ganesh\\src\\Imports.txt");
+
+        try {
+            FileReader fileReader = new FileReader(students);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        } catch (IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+        }
     }
+
 }
---
+++ src/com/company/Read.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+public class Read {
+
+
+
+}
